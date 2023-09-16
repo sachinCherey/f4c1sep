99922f6f3ef81fa53717187407920aaddabbbea4
Sachin_Cherey <64123014+sachinCherey@users.noreply.github.com>
2023-09-16 11:01:07 +0000

    try2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import  ReactDOM  from "react-dom";
-import App from '/workspaces/f4c1sep/src/App.js';
+import App from './App';
 
 ReactDOM.render(<App/>,document.getElementById('root'));
